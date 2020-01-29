3dca26af5f35eddbf72953ae2771491d0a76385e
Nigel Fox Runnels-Moss <nigel.runnels-moss@simplybusiness.co.uk>
2020-01-29 16:34:06 +0000

    Move tests out into their own file

--- ruby/robot.rb
+++ ruby/robot.rb
@@ -1,5 +1,3 @@
-require "minitest/autorun"
-
 class Stacks
   attr_accessor :stack
 
@@ -61,75 +59,6 @@ class Stacks
       true
     else
       false
-    end
-  end
-end
-
-describe "Stacks" do
-  describe "A new set of stacks" do
-    it "should have two elements" do
-      my_stack = Stacks.new(2)
-      _(my_stack.size()).must_equal 2
-    end
-
-    it "should be possible to find a block" do
-      my_stack = Stacks.new(2)
-      _(my_stack.find(0)).must_equal 0
-      _(my_stack.find(1)).must_equal 1
-    end
-
-    it "should not be possible to find a block that doesn't exist" do
-      my_stack = Stacks.new(2)
-      _(my_stack.find(2)).must_be_nil
-    end
-
-    it "should be possible to move a block from one index to another" do
-      my_stack = Stacks.new(2)
-      success = my_stack.move(1, 0)
-      _(success).must_equal true
-      _(my_stack.find(0)).must_equal 0
-      _(my_stack.find(1)).must_equal 0
-    end
-
-    it "should not be possible to move a block to a non-existent stack" do
-      my_stack = Stacks.new(2)
-      success = my_stack.move(1,3)
-      _(success).must_equal false
-    end
-
-    it "when two blocks are stacked we can find out what is on top of a block" do
-      my_stack = Stacks.new(2)
-      my_stack.move(1, 0)
-      _(my_stack.on_top(0)).must_equal [1]
-    end
-
-    it "when two blocks are stacked there is nothing on top of the top block" do
-      my_stack = Stacks.new(2)
-      my_stack.move(1, 0)
-      _(my_stack.on_top(1)).must_be_empty
-    end
-
-    it "when two blocks are stacked on_top returns nil for a non-existent block" do
-      my_stack = Stacks.new(2)
-      _(my_stack.on_top(-1)).must_be_nil
-    end
-
-    it "should not be possible to move a block from underneath another block" do
-      my_stack = Stacks.new(2)
-      my_stack.move(1, 0)
-      _(my_stack.move(0, 1)).must_equal false
-    end
-
-    it "should be possible to return a block to its original position" do
-      my_stack = Stacks.new(2)
-      my_stack.move(1, 0)
-      _(my_stack.return_block(1)).must_equal true
-    end
-
-    it "should not be possible to return a block if there is another on top" do
-      my_stack = Stacks.new(2)
-      my_stack.move(1, 0)
-      _(my_stack.return_block(0)).must_equal false
     end
   end
 end
